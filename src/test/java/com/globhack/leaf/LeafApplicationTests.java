9a7436415725287a84f4ae83575249c52fca95d2
samuel villanueva <svillanueva@asesoftware.com>
2020-05-23 22:25:31 -0500

    Commit inicial

---
+++ src/test/java/com/globhack/leaf/LeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globhack.leaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
