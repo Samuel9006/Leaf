4bea6ff013ddbc25d0daa4300ac89bd565f8ffbc
daviddgch <65839530+daviddgch@users.noreply.github.com>
2020-05-23 23:03:08 -0500

    Prueba servicio rest

--- src/main/java/com/globhack/leaf/Controller/PruebaController.java
+++ src/main/java/com/globhack/leaf/Controller/PruebaController.java
@@ -1,4 +1,21 @@
 package com.globhack.leaf.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
 public class PruebaController {
+	
+	@GetMapping("/prueba")
+	public String metodoPrueba() {
+		String prueba = "prueba despliegue";
+		
+		
+		return prueba;
+		
+	}
 }
